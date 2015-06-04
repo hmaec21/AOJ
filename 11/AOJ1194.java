e6bca65bb848e3973fddb65431871c59f699d718
hmaec21 <hmaec21@gmail.com>
2015-06-04 12:31:04 +0900

    データの持ち方を改良
    http://www.acm-icpc-training.net/aoj-1194-vampire.htmlを参考に改良

--- 11/AOJ1194.java
+++ 11/AOJ1194.java
@@ -1,6 +1,5 @@
 // AOJ 1194 "バンパイア" (ICPC国内予選 2014 Problem C)
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class AOJ1194 {
     public static double INF = 100.0;
@@ -14,52 +13,23 @@ public class AOJ1194 {
                 break;
             }
             
-            // field[y][x]: (x-20, y-1)にシルエット(境界も含む)があればtrue
-            boolean[][] field = new boolean[41][81];
-            Arrays.fill(field[0], true); // 地面に陰があると見なす
-            
+            int[] field = new int[40]; // field[x]: 区間[x-20, x-19）地点の影の高さ
             for (int i = 0; i < n; i++) {
                 int xl = sc.nextInt();
                 int xr = sc.nextInt();
                 int h  = sc.nextInt();
                 
-                for (int j = 1; j <= 2 * h; j++) {
-                    Arrays.fill(field[j], (xl + 20) * 2, (xr + 20) * 2 + 1, true);
+                for (int j = xl + 20; j < xr + 20; j++) {
+                    field[j] = Math.max(field[j], h);
                 }
             }
-            //printField(field);
-            
+            //printHeight(field);
             double minTime = INF;
-            for (int i = 0; i < field.length; i += 2) {
-                for (int j = 0; j < field[i].length; j += 2) {
-                    if (isBorder(field, j, i)) {
-                        // シルエットの上下の境界ならば、円との交点を求める
-                        double time = crossTime(j/2 - 20, i/2, r);
-                        minTime = Math.min(minTime, time);
-                    }
-                }
+            for (int i = 0; i < field.length; i++) {
+                minTime = Math.min(minTime, crossTime(i - 20, field[i], r));
+                minTime = Math.min(minTime, crossTime(i + 1 - 20, field[i], r));
             }
             System.out.println(minTime);
-        }
-    }
-    
-    public static boolean isBorder(boolean[][] field, int x, int y) {
-        if (!field[y][x]) {
-            return false;
-        } else if (y == field.length - 1 || (x == 0 || x == field[y].length - 1) && !field[y+1][x]) {
-            return true;
-        } else if (!field[y+1][x]) {
-            return true;
-        } else {
-            int num = 0;
-            for (int i = 0; i <= 1; i++) {
-                for (int j = -1; j <= 1; j++) {
-                    if (x + j >= 0 && x + j < field[y+i].length && field[y+i][x+j]) {
-                        num++;
-                    }
-                }
-            }
-            return num == 5 ? true : false;
         }
     }
     
@@ -75,16 +45,9 @@ public class AOJ1194 {
         }
     }
     
-    public static void printField(boolean[][] field) {
-        for (int i = field.length - 1; i >= 0; i -= 2) {
-            for (int j = 0; j < field[i].length; j += 2) {
-                if (field[i][j]) {
-                    System.out.print("-");
-                } else {
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
+    public static void printHeight(int[] field) {
+        for (int i = 0; i < field.length; i++) {
+            System.out.println((i - 20) + "-" + ((i - 20) + 1) + ": " + field[i]);
         }
     }
 }
